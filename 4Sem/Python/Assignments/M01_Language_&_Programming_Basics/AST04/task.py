d4c5f718903bf2ef65a02d0af6bd7d9e90793280
srividyareddy-0092 <srividyareddy0092@gmail.com>
2026-03-02 15:45:55 +0530

    done

--- 4Sem/Python/Assignments/M01_Language_&_Programming_Basics/AST04/task.py
+++ 4Sem/Python/Assignments/M01_Language_&_Programming_Basics/AST04/task.py
@@ -1,6 +1,8 @@
 def Reverse_String(s: str) -> str:
-   pass
-
+      reversed_str = ""
+      for i in range(len(s)):
+            reversed_str += s[len(s)-1-i]
+      return reversed_str
 
 if __name__ == '__main__':
     s = input()
